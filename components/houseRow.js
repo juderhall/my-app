400b40c854f72394cb1ed786ef87c40601c2520a
Jude Hall <jude.rendel.hall@gmail.com>
2022-12-11 15:16:34 -0700

    Commit

--- components/houseRow.js
+++ components/houseRow.js
@@ -1,5 +1,13 @@
-const houseRow = () => {
+import currencyFormatter from "../helpers/currencyFormatter"
 
+const houseRow = ({house}) => {
+    return (
+        <tr>
+            <td>{house.address}</td>
+            <td>{house.country}</td>
+            <td>{currencyFormatter.format(house.price)}</td>
+        </tr>
+    )
 }
 
 export default houseRow
