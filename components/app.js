9edd5b31a53c73a64f256562cde83bf516b35216
Jude <jude.rendel.hall@gmail.com>
2022-12-10 19:05:27 -0700

    Commit

--- components/app.js
+++ components/app.js
@@ -1,7 +1,13 @@
 import Banner from "./banner"
+import HouseList from "./houseList"
 
 const App = () => {
-    return <Banner headerText="Providing houses all over the world!"/>
+    return (
+        <>
+            <Banner headerText="Providing houses all over the world!"/>
+            <HouseList />
+        </>
+    )
 }
 
 export default App
